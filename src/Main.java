0a29d9c32dcd75dbc521c273aae78bdf2c4ccf6f
user <athimbityana@gmail com>
2022-06-06 16:45:52 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        carInstances audi = new carInstances();
+
+        audi.cModel="A8";
+        audi.cTankSize = 45;
+        audi.cColor = "red";
+        audi.maxSpeed = 380;
+        audi.engiSize = 1.5;
+
+        audi.DisplayAttr();
+
+    }
+}
---
+++ src/carInstances.java
@@ -0,0 +1,14 @@
+public class carInstances {
+    String cModel;
+    int cTankSize;
+    String cColor;
+    double engiSize;
+    double maxSpeed;
+
+    public void DisplayAttr(){
+        System.out.println("the model of the car is " + " " + cModel);
+        System.out.println("the tank of the car is " + " " + maxSpeed);
+        System.out.println("the color of the car is " + " " + cColor);
+        System.out.println("the engine size of fhe car is " + " " + engiSize);
+    }
+}
